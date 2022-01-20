450ac55c071f62345f1d68531e635381dfdd6bd7
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2022-01-19 20:02:46 -0800

    CSV export

--- handlers.js
+++ handlers.js
@@ -29,4 +29,14 @@ module.exports = {
             res.sendStatus(404);
         }
     },
+
+    export_csv(req, res) {
+        let content = 'ID,NAME,DESCRIPTION,QUANTITY\n';
+        for (const entry of database.inventory.list()) {
+            content += `${entry.id},${entry.name},${entry.description},${entry.quantity}\n`;
+        }
+        res.contentType('text/csv');
+        res.set('Content-Disposition', 'attachment; filename=data.csv');
+        res.send(content)
+    }
 }
--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ const register_routes = server => {
     server.post('/items', handlers.add);
     server.patch('/items/:id', handlers.update);
     server.delete('/items/:id', handlers.delete);
+    server.get('/items/export', handlers.export_csv);
 };
 
 module.exports = register_routes;
